5e6ee45123094c5575ff741743c11a63bc575dad
Madhav Jangid <manumadhavjangid@gmail.com>
2023-11-16 14:31:09 +0530

    nav bar for mobiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,38 @@ function Dispaly(sect){
     document.title = capitalize(sect)+' / X';
 }
 
-var flag = true
+var flag = false
 function ViewLeftNav(){
+    var navMenu = document.querySelector('#navMenu');   
     var leftNav = document.querySelector('.left-nav');   
     if(flag){
-        leftNav.style.left = '-200px';
+        navMenu.classList.remove('fa-regular','fa-circle-xmark');
+        navMenu.classList.add('bx','bx-menu');
+        leftNav.style.left = '-70px';
     }else{
+        navMenu.classList.remove('bx','bx-menu');
+        navMenu.classList.add('fa-regular','fa-circle-xmark');
         leftNav.style.left = '0';
     }
     flag = !flag;
 }
 
 
+var Following = document.querySelector('Following');
+var Followers = document.querySelector('Followers');
+
+Following.innerHTML = '0';
+Followers.innerHTML = '0';
+
+var NameOfuser = 'Username'
+var IdofUser = '@'+'UserID'
+var userName = document.querySelectorAll('.ProfileTopNavName');
+var userID = document.querySelectorAll('.useridhome');
+
+userName.forEach(element => {
+    element.innerHTML = NameOfuser;
+});
+
+userID.forEach(element => {
+    element.innerHTML = IdofUser;
+});
